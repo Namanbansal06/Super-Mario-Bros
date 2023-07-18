734ed9c4d6d821635faf6408b9d6c129e63a2c94
Naman bansal <bansalnaman44@gmail.com>
2023-07-18 23:06:24 +0530

    Mario Death Added

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public PlayerSpriteRenderer smallRenderer;
+    public PlayerSpriteRenderer bigRenderer;
+
+    private DeathAnimation deathAnimation;
+
+    public bool big => bigRenderer.enabled;
+    public bool small => smallRenderer.enabled;
+    public bool dead => deathAnimation.enabled;
+
+    private void Awake()
+    {
+        deathAnimation = GetComponent<DeathAnimation>();
+    }
+
+    public void Hit()
+    {
+        if (big)
+        {
+            Shrink();
+        }
+        else
+        {
+            Death();
+        }
+    }
+
+    private void Shrink()
+    {
+
+    }
+
+    private void Death()
+    {
+        smallRenderer.enabled = false;
+        bigRenderer.enabled = false;
+        deathAnimation.enabled = true;
+
+        GameManager.Instance.ResetLevel(3f);
+    }
+}
