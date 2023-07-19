affbc04848197736beed4c4f9ca3f97b33f35c1a
Naman bansal <bansalnaman44@gmail.com>
2023-07-19 21:20:29 +0530

    Mario Death Barrier Added

---
+++ Assets/Scripts/DeathBarrier.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathBarrier : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            collision.gameObject.SetActive(false);
+            GameManager.Instance.ResetLevel(3f);
+        }
+        else
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
