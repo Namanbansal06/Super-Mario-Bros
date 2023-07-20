0c776946847743664089bbc4d2d910e09a52e199
Naman bansal <bansalnaman44@gmail.com>
2023-07-21 00:46:54 +0530

    power ups added

---
+++ Assets/Scripts/PowerUp.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+    public enum Type
+    {
+        Coin,
+        ExtraLife,
+        MagicMushroom,
+        StarPower,
+    }
+
+    public Type type;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Collect(collision.gameObject);
+        }
+    }
+
+    private void Collect(GameObject player)
+    {
+        switch (type)
+        {
+            case Type.Coin:
+                GameManager.Instance.AddCoin();
+                break;
+            case Type.ExtraLife:
+                GameManager.Instance.AddLife();
+                break;
+            case Type.MagicMushroom:
+                player.GetComponent<Player>().Grow();
+                break;
+            case Type.StarPower:
+                player.GetComponent<Player>().StarPower();
+                break;
+
+        }
+
+        Destroy(gameObject);
+    }
+}
